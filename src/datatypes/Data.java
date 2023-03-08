e7dbde78a0790247ffbc2225691bf6ae93698710
Priyapius <126758696+Priyapius@users.noreply.github.com>
2023-03-08 12:48:00 +0530

    day1

---
+++ src/datatypes/Data.java
@@ -0,0 +1,28 @@
+package datatypes;
+
+public class Data {
+
+	public static void main(String[] args) {
+		boolean a=true;
+		byte b=120;
+		short c=-120;
+		int d=2468;
+		long e=-58963256485756l;
+		double f=-25.63;
+		float r=-40.6f;
+		char A='s';
+		String B="DATA_TYPES";
+		
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(r);
+		System.out.println(A);
+		System.out.println(B);
+	}
+
+}
