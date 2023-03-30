b8b975c8e0dd8940c0a7b59b4d8bb7e5ed40054f
Priyapius <126758696+Priyapius@users.noreply.github.com>
2023-03-30 12:31:33 +0530

    Conditions

---
+++ src/Conditions/program7.java
@@ -0,0 +1,24 @@
+package Conditions;
+
+public class program7 {
+
+	public static void main(String[] args) {
+		
+		for(int a=1;a<=6;a=a+1)
+		{
+			System.out.println("Have a nice day");
+			
+		}
+		
+		int b=1;
+		do
+		{
+			System.out.println("Bye");	
+			b=b+1;
+			
+		}while(b<=6);
+		}
+
+	}
+
+
